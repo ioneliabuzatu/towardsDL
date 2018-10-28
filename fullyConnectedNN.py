d6995df090999282d2e8adc304504b58bdd81190
Ionelia Buzatu <33060637+ioneliabuzatu@users.noreply.github.com>
2018-10-28 09:43:15 +0100

    Add files via upload

---
+++ fullyConnectedNN.py
@@ -0,0 +1,325 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from testCases_v2 import *
+import sklearn
+import sklearn.datasets
+import sklearn.linear_model
+from planar_utils import plot_decision_boundary, sigmoid, load_planar_dataset, load_extra_datasets
+
+# results are consistent
+np.random.seed(1)
+
+X, Y = load_planar_dataset()
+# Y contains lables 0=red and 1=blue
+# and X
+print(X.shape[0])
+print(Y.shape[0])
+print(X[1,1:4])
+print((X[1,1:4]).T)
+plt.scatter(X[0, :], X[1, :], c=np.squeeze(Y), s=40, cmap=plt.cm.Spectral)
+plt.show()
+
+
+shape_X = X.shape
+shape_Y = Y.shape
+m = X.shape[1]  # training set size
+### END CODE HERE ###
+
+print ('The shape of X is: ' + str(shape_X))
+print ('The shape of Y is: ' + str(shape_Y))
+print ('I have m = %d training examples!' % (m))
+
+# Train the logistic regression classifier
+clf = sklearn.linear_model.LogisticRegressionCV();
+print(clf.fit(X.T, Y.T))
+
+# Plot the decision boundary for logistic regression
+plot_decision_boundary(lambda x: clf.predict(x), X, Y.ravel())
+plt.title("Logistic Regression")
+plt.show()
+
+# Print accuracy
+LR_predictions = clf.predict(X.T)
+print ('Accuracy of logistic regression: %d ' % float((np.dot(Y,LR_predictions) + np.dot(1-Y,1-LR_predictions))/float(Y.size)*100) +
+       '% ' + "(percentage of correctly labelled datapoints)")
+
+
+# NN with one layer starts here
+
+def layer_sizes(X,Y):
+    """
+        Arguments:
+        X -- input dataset of shape (input size, number of examples)
+        Y -- labels of shape (output size, number of examples)
+
+        Returns:
+        n_x -- the size of the input layer
+        n_h -- the size of the hidden layer
+        n_y -- the size of the output layer
+        """
+    n_x = X.shape[0]
+    n_h =  4
+    n_y =  Y.shape[0]
+
+    return n_x, n_h, n_y
+
+
+n_x, n_h, n_y = layer_sizes(X,Y)
+print(n_x, n_h, n_y)
+
+def initialize_parameters(n_x, n_h, n_y):
+    """
+        Argument:
+        n_x -- size of the input layer
+        n_h -- size of the hidden layer
+        n_y -- size of the output layer
+
+        Returns:
+        params -- python dictionary containing your parameters:
+                        W1 -- weight matrix of shape (n_h, n_x)
+                        b1 -- bias vector of shape (n_h, 1)
+                        W2 -- weight matrix of shape (n_y, n_h)
+                        b2 -- bias vector of shape (n_y, 1)
+        """
+
+    W1 = np.random.randn(n_h, n_x) * 0.01
+    b1 = np.zeros((n_h, 1))
+    W2 = np.random.randn(n_y, n_h)
+    b2 = np.zeros((n_y,1))
+
+    assert (W1.shape == (n_h, n_x))
+    assert (b1.shape == (n_h, 1))
+    assert (W2.shape == (n_y, n_h))
+    assert (b2.shape == (n_y, 1))
+
+    parameters = {"W1": W1, "b1":b1, "W2":W2, "b2":b2}
+
+    return parameters
+
+
+n_x, n_h, n_y = initialize_parameters_test_case()
+
+parameters = initialize_parameters(n_x, n_h, n_y)
+print("W1 = " + str(parameters["W1"]))
+print("b1 = " + str(parameters["b1"]))
+print("W2 = " + str(parameters["W2"]))
+print("b2 = " + str(parameters["b2"]))
+
+
+def forward_propagation(X, parameters):
+    """
+        Argument:
+        X -- input data of size (n_x, m)
+        parameters -- python dictionary containing your parameters (output of initialization function)
+
+        Returns:
+        A2 -- The sigmoid output of the second activation
+        cache -- a dictionary containing "Z1", "A1", "Z2" and "A2"
+        """
+
+    # retrive the paramaters from the initialize_paramters() saved output
+    W1 = parameters["W1"]
+    b1 = parameters["b1"]
+    W2 = parameters["W2"]
+    b2 = parameters["b2"]
+
+    # farward propagation with random weights and 0 bias
+    Z1 = np.dot(W1, X)+b1
+    A1 = np.tanh(Z1)
+    Z2 = np.dot(W2, A1)+b2
+    A2 = sigmoid(Z2)
+
+    assert (A2.shape == (1, X.shape[1]))
+
+    cache = {"Z1":Z1, "A1":A1, "Z2":Z2, "A2":A2}
+
+    return A2, cache
+
+X_assess, parameters = forward_propagation_test_case()
+A2, cache = forward_propagation(X_assess, parameters)
+
+# Note: we use the mean here just to make sure that your output matches ours.
+print(np.mean(cache['Z1']) ,np.mean(cache['A1']),np.mean(cache['Z2']),np.mean(cache['A2']))
+
+def compute_cost(A2, Y, parameteres):
+    """
+        Computes the cross-entropy cost given in equation (13)
+
+        Arguments:
+        A2 -- The sigmoid output of the second activation, of shape (1, number of examples)
+        Y -- "true" labels vector of shape (1, number of examples)
+        parameters -- python dictionary containing your parameters W1, b1, W2 and b2
+
+        Returns:
+        cost -- cross-entropy cost given equation (13)
+        """
+
+    m = Y.shape[1]
+    cost = (-1 / m) * np.sum(Y * np.log(A2) + (1 - Y) * (np.log(1 - A2)))
+    cost = np.squeeze(cost)
+
+    return cost
+
+A2, Y_assess, parameters = compute_cost_test_case()
+
+print("cost = " + str(compute_cost(A2, Y_assess, parameters)))
+
+def backward_propagation(parameters, cache, X, Y):
+    """
+        Implement the backward propagation using the instructions above.
+
+        Arguments:
+        parameters -- python dictionary containing our parameters
+        cache -- a dictionary containing "Z1", "A1", "Z2" and "A2".
+        X -- input data of shape (2, number of examples)
+        Y -- "true" labels vector of shape (1, number of examples)
+
+        Returns:
+        grads -- python dictionary containing your gradients with respect to different parameters
+        """
+    A1 = cache["A1"]
+    A2 = cache["A2"]
+
+    W1 = parameters["W1"]
+    W2 = parameters["W2"]
+
+    m = X.shape[1]
+
+    dZ2 = A2 - Y
+    dW2 = (1/m)*np.dot(dZ2, A1.T)
+    db2 = (1/m) * np.sum(dZ2, axis = 1, keepdims = True)
+    dZ1 = np.dot(W2.T,dZ2) * (1 - np.power(A1,2))
+    dW1 = (1/m)*np.dot(dZ1, X.T)
+    db1 = (1/m)*np.sum(dZ1, axis = 1, keepdims = True)
+
+    grads = {"dW1":dW1,
+             "db1":db1,
+             "dW2":dW2,
+             "db2":db2}
+
+    return grads
+
+parameters, cache, X_assess, Y_assess = backward_propagation_test_case()
+
+grads = backward_propagation(parameters, cache, X_assess, Y_assess)
+print ("dW1 = "+ str(grads["dW1"]))
+print ("db1 = "+ str(grads["db1"]))
+print ("dW2 = "+ str(grads["dW2"]))
+print ("db2 = "+ str(grads["db2"]))
+
+
+def update_parameters(parameters, grads, learning_rate = 1.2):
+    """
+        Updates parameters using the gradient descent update rule given above
+
+        Arguments:
+        parameters -- python dictionary containing your parameters
+        grads -- python dictionary containing your gradients
+
+        Returns:
+        parameters -- python dictionary containing your updated parameters
+        """
+
+    W1 = parameters["W1"] - learning_rate * grads["dW1"]
+    W2 = parameters["W2"] - learning_rate * grads["dW2"]
+    b1 = parameters["b1"] - learning_rate * grads["db1"]
+    b2 = parameters["b2"] - learning_rate * grads["db2"]
+
+    parameters = {"W1":W1,
+                  "b1":b1,
+                  "W2":W2,
+                  "b2":b2}
+
+    return parameters
+
+parameters, grads = update_parameters_test_case()
+parameters = update_parameters(parameters, grads)
+
+print("W1 = " + str(parameters["W1"]))
+print("b1 = " + str(parameters["b1"]))
+print("W2 = " + str(parameters["W2"]))
+print("b2 = " + str(parameters["b2"]))
+
+def nn_model(X, Y, n_h, num_iterations = 10000, print_cost=False):
+    """
+        Arguments:
+        X -- dataset of shape (2, number of examples)
+        Y -- labels of shape (1, number of examples)
+        n_h -- size of the hidden layer
+        num_iterations -- Number of iterations in gradient descent loop
+        print_cost -- if True, print the cost every 1000 iterations
+
+        Returns:
+        parameters -- parameters learnt by the model. They can then be used to predict.
+        """
+    np.random.seed(3)
+
+    n_x = layer_sizes(X, Y)[0]
+    n_y = layer_sizes(X,Y)[2]
+
+    parameters = initialize_parameters(n_x, n_h, n_y)
+    W1 = parameters["W1"]
+    b1 = parameters["b1"]
+    W2 = parameters["W2"]
+    b2 = parameters["b2"]
+
+    for i in range(num_iterations):
+
+        A2, cache = forward_propagation(X, parameters)
+        cost = compute_cost(A2, Y, parameters)
+        grads  = backward_propagation(parameters, cache, X, Y)
+        parameters = update_parameters(parameters, grads, learning_rate = 1.2)
+
+    return parameters
+
+
+X_assess, Y_assess = nn_model_test_case()
+parameters = nn_model(X_assess, Y_assess, 4, num_iterations=10000, print_cost=True)
+print("W1 = " + str(parameters["W1"]))
+print("b1 = " + str(parameters["b1"]))
+print("W2 = " + str(parameters["W2"]))
+print("b2 = " + str(parameters["b2"]))
+
+
+def predict(parameters, X):
+    """
+        Using the learned parameters, predicts a class for each example in X
+
+        Arguments:
+        parameters -- python dictionary containing your parameters
+        X -- input data of size (n_x, m)
+
+        Returns
+        predictions -- vector of predictions of our model (red: 0 / blue: 1)
+        """
+
+    A2 , cache = forward_propagation(X, parameters)
+
+    predictions = np.round(A2)
+
+    return predictions
+
+
+parameters, X_assess = predict_test_case()
+
+predictions = predict(parameters, X_assess)
+print("predictions mean = " + str(np.mean(predictions)))
+
+parameters = nn_model(X, Y, n_h = 4, num_iterations = 10000, print_cost=True)
+
+# Print accuracy
+predictions = predict(parameters, X)
+
+print ('Accuracy: %d' % float((np.dot(Y,predictions.T) + np.dot(1-Y,1-predictions.T))/float(Y.size)*100) + '%')
+
+plt.figure(figsize=(16, 32))
+hidden_layer_sizes = [1, 2, 3, 4, 5, 20, 50]
+for i, n_h in enumerate(hidden_layer_sizes):
+    plt.subplot(5, 2, i+1)
+    plt.title('Hidden Layer of size %d' % n_h)
+    parameters = nn_model(X, Y, n_h, num_iterations = 5000)
+    plot_decision_boundary(lambda x: predict(parameters, x.T), X, Y.ravel())
+    predictions = predict(parameters, X)
+    accuracy = float((np.dot(Y,predictions.T) + np.dot(1-Y,1-predictions.T))/float(Y.size)*100)
+    print ("Accuracy for {} hidden units: {} %".format(n_h, accuracy))
+plt.savefig("n_hidden_layers")
